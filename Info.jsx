ababde8ffe03163c879a29342077bd96363b5dee
veda20 <71143525+veda20@users.noreply.github.com>
2021-06-27 15:05:53 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p> A basic web development bootcamp . This is a bootcamp taken up by Shaurya Sinha. In the end we were able to build a simple website.</p>
+    </div>
+  );
+}
+export default Info;
